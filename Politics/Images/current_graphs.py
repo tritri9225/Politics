6571ff6f4f767e221854aa1f7f12152651bf57ca
evelynlawrie <53349793+evelynlawrie@users.noreply.github.com>
2019-08-02 14:17:38 -0400

    Add files via upload

---
+++ Politics/Images/current_graphs.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = {'Bennet' : 0.2,
+'Biden': 32.0,
+'Booker' : 1.6,
+'Bullock' : 0.2,
+'Buttigieg' : 5.6,
+'Castro' : 1.0,
+'de Blasio' : 0.4,
+'Delaney' : 0.8,
+'Gabbard' : 1.0,
+'Gillibrand' : 0.4,
+'Harris' : 11.0,
+'Hickenlooper' : 0.4,
+'Klobuchar' : 0.6,
+"O'Rourke" : 3.0,
+'Ryan' : 0.6,
+'Sanders' : 16.4,
+'Steyer' : 0.8,
+'Warren' : 14.8,
+'Williamson' : 0.4,
+'Yang' : 1.6}
+
+names = ['Bennet','Biden','Booker','Bullock','Buttigieg','Castro','de Blasio','Delaney','Gabbard','Gillibrand','Harris','Hickenlooper'Inslee
+Klobuchar
+Messam
+Moulton
+O’Rourke
+Ryan
+Sanders
+Sestak
+Steyer
+Warren
+Williamson
+Yang
+]
+values = []
+bars = names
+height values
+y_pos = np.arange(len(bars))
