0c7401994d3b8d1cfdf9755a3f11a526d48cf87e
evelynlawrie <53349793+evelynlawrie@users.noreply.github.com>
2019-08-03 16:26:12 -0400

    Add files via upload

--- Politics/Images/current_graphs.py
+++ Politics/Images/current_graphs.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = {'Bennet' : 0.2,
+'''data = {'Bennet' : 0.2,
 'Biden': 32.0,
 'Booker' : 1.6,
 'Bullock' : 0.2,
@@ -20,22 +20,29 @@ data = {'Bennet' : 0.2,
 'Steyer' : 0.8,
 'Warren' : 14.8,
 'Williamson' : 0.4,
-'Yang' : 1.6}
+'Yang' : 1.6}'''
 
-names = ['Bennet','Biden','Booker','Bullock','Buttigieg','Castro','de Blasio','Delaney','Gabbard','Gillibrand','Harris','Hickenlooper'Inslee
-Klobuchar
-Messam
-Moulton
-O’Rourke
-Ryan
-Sanders
-Sestak
-Steyer
-Warren
-Williamson
-Yang
-]
-values = []
+names = ['Bennet','Biden','Booker','Bullock','Buttigieg','Castro','de Blasio','Delaney','Gabbard','Gillibrand','Harris','Hickenlooper','Inslee','Klobuchar','Messam','Moulton','O’Rourke','Ryan','Sanders','Sestak','Steyer','Warren','Williamson','Yang']
+values = [0.2,32.0,1.6,0.2,5.6,1.0,0.4,0.8,1.0,0.4,11.0,0.4,0.6,3.0,0.6,16.4,0.8,14.8,0.4,1.6]
+
 bars = names
-height values
+height = values
 y_pos = np.arange(len(bars))
+bar_plot = plt.bar(y_pos, height, color='blue')
+plt.xticks(y_pos, bars)
+#plt.yticks([0, 0.5, 5, 50], ['0%', '0.5%', '5%', '50%'])
+plt.title('Current Poll Data of Democratic Candidate Popularity')
+plt.xlabel('Candidates')
+plt.ylabel('Percentage of Voter Support')
+
+plt.text
+
+#create labels for each bar
+def autolabel(i):
+    for i in enumerate(bar_plot):
+        height = i.get_height()
+        plt.text(i.get_x() + i.get_width()/2, 1.05*height, bar_label(values[i]), ha='center', va='bottom', rotation=90)
+
+autolabel(bar_plot)
+
+plt.show()
